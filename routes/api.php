52e2f9732688c5432b168b3f07f75c4bde610cce
Nick Ezekiel <nickezekiel@ignislab.com>
2024-07-01 02:30:04 +0100

    added v1 route group

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,14 @@ Route::get('/home', function () {
     return response()->json(['error' => 'Already authenticated'], 200);
 });
 
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/users/auth', function (Request $request) {
+            return $request->user();
+        });
+    });
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
